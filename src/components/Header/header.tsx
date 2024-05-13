60523d85916fb26ae12d5aadb0dbfc788dbefab4
Lucas Eiji <lucas09.eiji@gmail.com>
2024-05-13 19:27:44 -0300

    first-commit

---
+++ src/components/Header/header.tsx
@@ -0,0 +1,14 @@
+import "./header.scss"
+
+export default function Header() {
+    return (
+        <section className="header">
+            <h1 className="header_title">BMR CALCULATOR</h1>
+
+            <div className="navigation">
+                <a href="">Contact</a>
+                <a href="">Help</a>
+            </div>
+        </section>
+    )
+}
